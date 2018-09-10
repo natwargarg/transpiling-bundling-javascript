3dcfa4d25d7ca183717e946d3c6fb38803f3aea1
Garg <natwar.garg@capgemini.com>
2018-09-10 16:05:48 +0200

    Preparation

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+            app: './src/index.js',
+            print: './src/print.js'
+        },
+        plugins: [
+             new HtmlWebpackPlugin({
+               title: 'Output Management'
+             })
+           ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
